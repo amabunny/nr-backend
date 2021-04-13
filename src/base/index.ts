1530c7790f7f6913c80f90adb7e9decfa7d5ef86
amabunny <iamgreyy@gmail.com>
2021-04-13 09:58:06 +0500

    Some puppeteer fixes

--- src/base/index.ts
+++ src/base/index.ts
@@ -7,8 +7,7 @@ import { DpsProvider } from './providers';
   imports: [
     PuppeteerModule.forRoot({
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-      args: ['--no-sandbox', '--lang=ru,ru-RU'],
-    } as any),
+    }),
   ],
   controllers: [DpsController],
   providers: [DpsProvider],
